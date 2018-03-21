2edd7f7e410120b77543f45aa619cac2618aff58
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-21 18:33:12 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,17 @@
 class Hero
-  
+  attr_accessor :name, :bio, :power 
+
+  @@all = []
+
+  def initialize(name, bio, power)
+    @name = name 
+    @bio = bio
+    @power = power
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
