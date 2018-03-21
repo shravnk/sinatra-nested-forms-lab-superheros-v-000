92bcedf8d4033c99942b91b1725b4d6770071584
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-21 18:41:53 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,15 +1,14 @@
 class Team
-  attr_accessor :name, :motto
-
-  @@heroes
+  attr_accessor :name, :motto, :heroes
 
   def initialize(name, motto)
     @name = name
     @motto = motto
+    @heros = []
   end
 
   def self.import_heroes(heroes)
-    heroes.each{|h| @@heroes << h}
+    heroes.each{|h| @heroes << h}
   end
 
 end
